19a72d526362b6515b756104aa3e9d63b2aacb35
Oliver Payne <opayne@ripe.net>
2014-08-25 10:51:35 +0200

    tested up to 3.9.2

--- varnish-http-purge.php
+++ varnish-http-purge.php
@@ -40,7 +40,7 @@ class VarnishPurger {
                 'zip_url' => 'https://github.com/olipayne/varnish-http-purge-multi/zipball/master', // the zip url of the github repo
                 'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
                 'requires' => '3.0', // which version of WordPress does your plugin require?
-                'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+                'tested' => '3.9.2', // which version of WordPress is your plugin tested up to?
                 'readme' => 'README.md' // which file to use as the readme for the version number
             );
             new WP_GitHub_Updater($config);

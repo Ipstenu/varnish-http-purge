5735fbb5176e08229eb44d80fcfdb0a42fa52468
Oliver Payne <opayne@ripe.net>
2014-08-25 10:40:49 +0200

    added updated.php and set variables

--- varnish-http-purge.php
+++ varnish-http-purge.php
@@ -28,18 +28,20 @@ class VarnishPurger {
     protected $purgeUrls = array();
 
     public function __construct() {
+
         if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+            require_once('updater.php');
             $config = array(
                 'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
                 'proper_folder_name' => 'varnish-http-purge', // this is the name of the folder your plugin lives in
-                'api_url' => 'https://api.github.com/repos/username/repository-name', // the github API url of your github repo
-                'raw_url' => 'https://raw.github.com/username/repository-name/master', // the github raw url of your github repo
-                'github_url' => 'https://github.com/username/repository-name', // the github url of your github repo
-                'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the github repo
-                'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+                'api_url' => 'https://api.github.com/repos/olipayne/varnish-http-purge', // the github API url of your github repo
+                'raw_url' => 'https://raw.github.com/olipayne/varnish-http-purge/master', // the github raw url of your github repo
+                'github_url' => 'https://github.com/olipayne/varnish-http-purge', // the github url of your github repo
+                'zip_url' => 'https://github.com/olipayne/varnish-http-purge/zipball/master', // the zip url of the github repo
+                'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
                 'requires' => '3.0', // which version of WordPress does your plugin require?
                 'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-                'readme' => 'README.MD' // which file to use as the readme for the version number
+                'readme' => 'README.md' // which file to use as the readme for the version number
             );
             new WPGitHubUpdater($config);
         }

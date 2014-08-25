a474c4636686ed4ab2b1fbd894657c32b3ac44e3
Oliver Payne <opayne@ripe.net>
2014-08-25 10:46:44 +0200

    updated plugin name

--- varnish-http-purge.php
+++ varnish-http-purge.php
@@ -33,11 +33,11 @@ class VarnishPurger {
             require_once('updater.php');
             $config = array(
                 'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-                'proper_folder_name' => 'varnish-http-purge', // this is the name of the folder your plugin lives in
-                'api_url' => 'https://api.github.com/repos/olipayne/varnish-http-purge', // the github API url of your github repo
-                'raw_url' => 'https://raw.github.com/olipayne/varnish-http-purge/master', // the github raw url of your github repo
-                'github_url' => 'https://github.com/olipayne/varnish-http-purge', // the github url of your github repo
-                'zip_url' => 'https://github.com/olipayne/varnish-http-purge/zipball/master', // the zip url of the github repo
+                'proper_folder_name' => 'varnish-http-purge-multi', // this is the name of the folder your plugin lives in
+                'api_url' => 'https://api.github.com/repos/olipayne/varnish-http-purge-multi', // the github API url of your github repo
+                'raw_url' => 'https://raw.github.com/olipayne/varnish-http-purge-multi/master', // the github raw url of your github repo
+                'github_url' => 'https://github.com/olipayne/varnish-http-purge-multi', // the github url of your github repo
+                'zip_url' => 'https://github.com/olipayne/varnish-http-purge-multi/zipball/master', // the zip url of the github repo
                 'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
                 'requires' => '3.0', // which version of WordPress does your plugin require?
                 'tested' => '3.3', // which version of WordPress is your plugin tested up to?
